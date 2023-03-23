ac96d43f9d41020dfd5ea506e2874dd70b61fdc4
kiyooteru <kiyooteru@gmail.com>
2023-03-23 09:37:16 +0100

    Created the admin category page

---
+++ admin/categories.php
@@ -0,0 +1,61 @@
+<?php include "includes/header.php" ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+       <?php include "includes/navigation.php" ?>
+
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to admin 
+                            <small>Subheading</small>
+                        </h1>
+
+<div class="col-xs-6">
+<form action="">
+<div class="form-group">
+    <label for="cat-title">Add Category</label>
+<input type="text" class="form-control" name="cat_title">
+</div>
+<div class="form-group">
+    <input class="btn btn-primary" type="submit" name="submit" value="Add category">
+</div>
+</form>
+</div>
+
+<div class="col-xs-6">
+    <table class="table table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Category title</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>basketball category</td>
+                <td>baseball category</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+                    
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+
+        <!-- /#page-wrapper -->
+<?php include "includes/footer.php" ?>
