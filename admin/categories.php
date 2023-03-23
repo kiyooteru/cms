44235140e402030603b1c3e4711223fca266210c
kiyooteru <kiyooteru@gmail.com>
2023-03-23 14:36:10 +0100

    Added "delete" functionality

--- admin/categories.php
+++ admin/categories.php
@@ -51,11 +51,6 @@ if(!$create_category_query) {
 
 <div class="col-xs-6">
 
-<?php
-$query = "SELECT * FROM categories";
-$select_categories = mysqli_query($connection, $query );
-?>
-
 
     <table class="table table-bordered table-hover">
         <thead>
@@ -66,16 +61,36 @@ $select_categories = mysqli_query($connection, $query );
         </thead>
         <tbody>
 
-<?php
+<?php   // FIND ALL CATEGORIES QUERY
+
+$query = "SELECT * FROM categories";
+$select_categories = mysqli_query($connection, $query );
+
+
 while($row = mysqli_fetch_assoc($select_categories)) {
         $cat_id = $row['cat_id'];
     $cat_title = $row['cat_title'];
 echo "<tr>";
     echo "<td>{$cat_id}</td>";
-        echo "<td>{$cat_title}</td>";
+    echo "<td>{$cat_title}</td>";
+    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+
 echo "</tr> ";
 }
 ?>
+
+<?php  // DELETE QUERY
+if(isset($_GET['delete'])) {
+$the_cat_id = $_GET['delete'];
+$query = "DELETE from categories WHERE cat_id = {$the_cat_id} ";
+$delete_query = mysqli_query($connection, $query);
+header("Location: categories.php");
+}
+
+?>
+
+
+
         </tbody>
     </table>
 </div>
